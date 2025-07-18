a81db23c1e2f1edaf076848117ee991501d9e1a5
Ritesh Kumar <ritesh.iitkgp32@gmail.com>
2025-07-18 17:23:33 +0530

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'fe9e5aac0bmshd7e65d7a2d4c1eap121406jsn6d700cef01f4',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "107f6e7ecc88ad1c99a9d27068efff6a";
